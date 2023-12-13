30a9c4860a625445a36ca03f29bddcbce5f5add3
glebs <glebs@GLEB-PC>
2023-12-13 16:46:23 +0300

    Добавьте файлы проекта.

---
+++ Следующее и предыдущее число/Следующее и предыдущее число.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	int a;
+	
+	cout << "First num: \n";
+	cin >> a;
+
+
+	cout << "The next number for the number "<< a << " is " << a + 1 <<"." << endl;
+	cout << "The previous number for the number " << a << " is " << a - 1 << "." << endl;
+}
